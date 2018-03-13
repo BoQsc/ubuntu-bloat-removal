865f6bb630a78efc5870b72fbbe6b35b383ce47d
ItsTimeForABit <19717159+aFunnyBit@users.noreply.github.com>
2018-03-13 01:09:10 -0500

    Update 17_04-ubuntu-bloat-remover.sh

--- 17_04-ubuntu-bloat-remover.sh
+++ 17_04-ubuntu-bloat-remover.sh
@@ -1,7 +1,84 @@
 #!/bin/bash
 
 
-apt-get remove account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo aisleriot brltty colord deja-dup duplicity empathy empathy-common evolution-data-server-online-accounts example-content firefox gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common mcp-account-manager-uoa python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut thunderbird thunderbird-gnome-support totem totem-common totem-plugins unity-scope-chromiumbookmarks unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-manpages unity-scope-musicstores unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero
+apt-get remove account-plugin-facebook -y
+apt-get remove account-plugin-jabber -y
+apt-get remove account-plugin-salut -y
+apt-get remove account-plugin-twitter -y
+apt-get remove account-plugin-windows-live-y
+apt-get remove account-plugin-yahoo -y
+apt-get remove aisleriot -y
+apt-get remove brltty -y
+apt-get remove colord -y
+apt-get remove deja-dup -y
+apt-get remove duplicity -y
+apt-get remove empathy -y
+apt-get remove empathy-common -y
+apt-get remove evolution-data-server-online-accounts -y
+apt-get remove example-content -y
+#apt-get remove firefox -y
+apt-get remove gnome-accessibility-themes -y
+apt-get remove gnome-contacts -y
+apt-get remove gnome-mahjongg -y
+apt-get remove gnome-mines -y
+apt-get remove gnome-orca -y
+apt-get remove gnome-screensaver -y
+apt-get remove gnome-sudoku -y
+apt-get remove gnome-video-effects -y
+apt-get remove gnomine -y
+apt-get remove landscape-common -y
+apt-get remove libreoffice-avmedia-backend-gstreamer -y
+apt-get remove libreoffice-base-core -y
+apt-get remove libreoffice-calc -y
+apt-get remove libreoffice-common -y
+apt-get remove libreoffice-core -y
+apt-get remove libreoffice-draw -y
+apt-get remove libreoffice-gnome -y
+apt-get remove libreoffice-gtk -y
+apt-get remove libreoffice-impress -y
+apt-get remove libreoffice-math -y
+apt-get remove libreoffice-ogltrans -y
+apt-get remove libreoffice-pdfimport -y
+apt-get remove libreoffice-style-galaxy -y
+apt-get remove libreoffice-style-human -y
+apt-get remove libreoffice-writer -y
+apt-get remove libsane -y
+apt-get remove libsane-common -y
+apt-get remove mcp-account-manager-uoa -y
+apt-get remove python3-uno -y
+apt-get remove rhythmbox -y
+apt-get remove rhythmbox-plugins -y
+apt-get remove rhythmbox-plugin-zeitgeist -y
+apt-get remove sane-utils -y
+apt-get remove shotwell -y
+apt-get remove shotwell-common -y
+apt-get remove telepathy-gabble -y
+apt-get remove telepathy-haze -y 
+apt-get remove telepathy-idle -y
+apt-get remove telepathy-indicator -y
+apt-get remove telepathy-logger -y
+apt-get remove telepathy-mission-control-5 -y
+apt-get remove telepathy-salut -y
+apt-get remove thunderbird -y
+apt-get remove thunderbird-gnome-support -y
+apt-get remove totem -y
+apt-get remove totem-common -y
+apt-get remove totem-plugins -y
+apt-get remove unity-scope-chromiumbookmarks -y
+apt-get remove unity-scope-colourlovers -y
+apt-get remove unity-scope-devhelp -y
+apt-get remove unity-scope-firefoxbookmarks -y
+apt-get remove unity-scope-manpages -y
+apt-get remove unity-scope-musicstores -y
+apt-get remove unity-scope-openclipart -y
+apt-get remove unity-scope-texdoc -y
+apt-get remove unity-scope-tomboy -y
+apt-get remove unity-scope-video-remote -y
+apt-get remove unity-scope-virtualbox -y
+apt-get remove unity-scope-yelp -y
+apt-get remove unity-scope-zotero -y
 
+# Enable the debug port if available.
+# systemctl status serial-getty@ttyS0.service
 systemctl enable serial-getty@ttyS2.service
 systemctl start serial-getty@ttyS2.service
